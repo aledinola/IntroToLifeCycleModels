f78d5ff9843a862aba99fab320f370e91a9743d9
Alessandro Di Nola <40523651+aledinola@users.noreply.github.com>
2024-12-07 12:40:04 +0000

    small update

--- ALE/AleModel24.m
+++ ALE/AleModel24.m
@@ -27,7 +27,8 @@ n_d = 11;    % Endogenous labor choice (fraction of time worked)
 n_a = 201;   % Endogenous asset holdings
 n_z = [7,2]; % (1) labor prod shock, (2) health shock
 n_e = 3;     % iid shock
-N_i = 2;     % Permanent type of agents
+N_i = {'low','high'};     % Permanent type of agents
+n_theta = numel(N_i);
 N_j = Params.J; % Number of periods in finite horizon
 
 %% The parameter that depends on the permanent type
@@ -90,22 +91,41 @@ z1_grid=exp(z1_grid); % Take exponential of the grid
 [mean_z1,~,~,~]=MarkovChainMoments(z1_grid,pi_z1); % Calculate the mean of the grid so as can normalise it
 z1_grid=z1_grid/mean_z1; % Normalise the grid on z (so that the mean of z is 1)
 
-% Second, the health shock z2, which is age-dependent
+%% Second, the health shock z2, which is age-dependent
+% (1) The transition prob. into "health=sick" is increasing in age: Older 
+%     individuals are more likely to be sick
+% (2) The transition into sick is higher for the low type rel. to the high
+%     type
 Params.cut = 0.5; % productivity cut if health is bad
 z2_grid = [0,1]'; %0=bad health, 1=good health
 p_bb_j = linspace(0.3,0.6,N_j); % prob from bad health to bad health
-p_gb_j = linspace(0.0,0.2,N_j); % prob from good health to bad health
-pi_z2_J = zeros(n_z(2),n_z(2),N_j);
+p_gb_j.high = linspace(0.0,0.2,N_j);  % prob from good health to bad health, high type
+p_gb_j.low  = linspace(0.1,0.4,N_j); % prob from good health to bad health, low type
+%p_gb_j.low  = p_gb_j.high;
+pi_z2_J.low  = zeros(n_z(2),n_z(2),N_j);
+pi_z2_J.high = zeros(n_z(2),n_z(2),N_j);
 for j=1:N_j
+    % Low type
     prob_j = [p_bb_j(j),1-p_bb_j(j);
-              p_gb_j(j),1-p_gb_j(j)];
+              p_gb_j.low(j),1-p_gb_j.low(j)];
     if any(abs(sum(prob_j,2)-1)>1e-10)
         fprintf('j = %d \n',j)
         error('prob_j does not sum to one!')
     end
-    pi_z2_J(:,:,j) = prob_j;
+    pi_z2_J.low(:,:,j) = prob_j;
+    % High type
+    prob_j = [p_bb_j(j),1-p_bb_j(j);
+              p_gb_j.high(j),1-p_gb_j.high(j)];
+    if any(abs(sum(prob_j,2)-1)>1e-10)
+        fprintf('j = %d \n',j)
+        error('prob_j does not sum to one!')
+    end
+    pi_z2_J.high(:,:,j) = prob_j;
 end
-check_markov_age(pi_z2_J,n_z(2),N_j);
+disp('Checking pi_z2_J.low...')
+check_markov_age(pi_z2_J.low,n_z(2),N_j);
+disp('Checking pi_z2_J.high...')
+check_markov_age(pi_z2_J.high,n_z(2),N_j);
 
 % Initial distribution of z2 at age 1, potentially different from the
 % stationary distribution of pi_z2
@@ -130,13 +150,15 @@ d_grid=h_grid;
 
 z_grid = [z1_grid;z2_grid]; % size is [n_z1+n_z2,1]
 % pi_z2_J is [nz2,nz2,J], pi_z1 is [nz1,nz1]
-pi_z1_J = repmat(pi_z1,[1,1,N_j]); % becomes [nz1,nz1,J]
-pi_z = zeros(prod(n_z),prod(n_z),N_j); % [nz1*nz2,nz1*nz2,J]
+pi_z.low  = zeros(prod(n_z),prod(n_z),N_j); % [nz1*nz2,nz1*nz2,J]
+pi_z.high = zeros(prod(n_z),prod(n_z),N_j); % [nz1*nz2,nz1*nz2,J]
 for j=1:N_j
-    pi_z(:,:,j) = kron(pi_z2_J(:,:,j),pi_z1_J(:,:,j)); % kron in reverse order
+    pi_z.low(:,:,j)  = kron(pi_z2_J.low(:,:,j),pi_z1);  % kron in reverse order
+    pi_z.high(:,:,j) = kron(pi_z2_J.high(:,:,j),pi_z1); % kron in reverse order
 end
-
-check_markov_age(pi_z,prod(n_z),N_j);
+disp('Checking pi_z.low and pi_z.high...')
+check_markov_age(pi_z.low,prod(n_z),N_j);
+check_markov_age(pi_z.high,prod(n_z),N_j);
 
 %% Now, create the return function 
 DiscountFactorParamNames={'beta','sj'};
@@ -155,10 +177,10 @@ vfoptions.verbose=1; % Just so we can see feedback on progress
 time_vfi=toc;
 
 disp('Check size of V:')
-disp(size(V.ptype001))
+disp(size(V.low))
 disp([n_a,n_z(1),n_z(2),n_e,N_j])
 disp('Check size of Policy:')
-disp(size(Policy.ptype001))
+disp(size(Policy.low))
 disp([length(n_d)+length(n_a),n_a,n_z(1),n_z(2),n_e,N_j])
 
 %% Initial distribution of agents at birth (j=1)
@@ -184,21 +206,27 @@ tic
 StatDist=StationaryDist_Case1_FHorz_PType(jequaloneDist,AgeWeightsParamNames,PTypeDistParamNames,Policy,n_d,n_a,n_z,N_j,N_i,pi_z,Params,simoptions);
 time_dist=toc;
 
-% Check that the distribution sums to one for each type
-sum(StatDist.ptype001,'all')
-sum(StatDist.ptype002,'all')
+disp('Check that the distribution sums to one for each type')
+sum(StatDist.low,'all')
+sum(StatDist.high,'all')
 
 % Convert distribution and policy functions from toolkit GPU to arrays on 
 % the cpu, adding the PT as an extra dimension in the arrays (instead of a 
 % structure as in the toolkit) 
 [mu,Policy_cpu] = reshape_VandPolicy(StatDist,Policy,n_a,n_z,n_e,N_i,N_j);
+
+% Check distribution
+mu1 = gather(StatDist.low);
+mu2 = squeeze(mu(:,:,:,:,1,:))/StatDist.ptweights(1);
+max(abs(mu1-mu2),[],"all")
+
 % State variables: (a,z1,z2,e,theta_i,j)
-mu_a = sum(mu,[2,3,4,5,6]);
-
-% figure
-% plot(a_grid,mu_a)
-% title('Distribution of assets')
-
+% mu_a = sum(mu,[2,3,4,5,6]);
+% 
+% % figure
+% % plot(a_grid,mu_a)
+% % title('Distribution of assets')
+% 
 %% FnsToEvaluate are how we say what we want to graph the life-cycles of
 % Like with return function, we have to include (h,aprime,a,z) as first
 % inputs, then just any relevant parameters.
@@ -215,16 +243,6 @@ condres.sick = @(h,aprime,a,z1,z2,e) (z2==0);
 condres.healthy = @(h,aprime,a,z1,z2,e) (z2==1);
 % Add additional field to simoptions
 simoptions.conditionalrestrictions = condres;
-
-%% Calculate Lorenz curve of assets manually
-Values_assets1 = repmat(a_grid,[1,n_z(1),n_z(2),n_e,N_i,N_j]);
-Values_assets1 = Values_assets1(:);
-StatDist1      = mu(:);
-
-%[relpop,relz] = mylorenz(Values_assets1,StatDist1);
-
-%figure
-%plot(relpop,relpop,relpop,relz)
 
 %% Calculate the life-cycle profiles
 disp('LifeCycleProfiles_FHorz_Case1_PType')
@@ -243,171 +261,240 @@ tic
 AllStats=EvalFnOnAgentDist_AllStats_FHorz_Case1_PType(StatDist, Policy, FnsToEvaluate, Params,n_d,n_a,n_z,N_j,N_i,d_grid, a_grid, z_grid, simoptions);
 time_all=toc;
 
-AllStats.assets.Gini
-
-%% Plot
-
+% %% Plot
+% 
 % Share of sick hourseholds
 figure
+plot(age_vec,AgeStats.share_sick.low.Mean)
+hold on
 plot(age_vec,AgeStats.share_sick.Mean)
-xlabel('Age, j')
-title('Share of sick households in the pop.')
-
-% Earnings
-figure
-plot(age_vec,AgeStats.sick.earnings.Mean)
 hold on
-plot(age_vec,AgeStats.earnings.Mean)
-hold on
-plot(age_vec,AgeStats.healthy.earnings.Mean)
-hold off
-title('Earnings by health status')
+plot(age_vec,AgeStats.share_sick.high.Mean)
 xlabel('Age, j')
-legend('Sick','All','Healthy')
+legend('Low type','All','High type')
+title('Share of sick households by age')
 
-% Worked hours h
-figure
-plot(age_vec,AgeStats.sick.hours.Mean)
-hold on
-plot(age_vec,AgeStats.hours.Mean)
-hold on
-plot(age_vec,AgeStats.healthy.hours.Mean)
-hold off
-title('Worked hours by health status')
-xlabel('Age, j')
-legend('Sick','All','Healthy')
+% % Earnings
+% figure
+% plot(age_vec,AgeStats.sick.earnings.Mean)
+% hold on
+% plot(age_vec,AgeStats.earnings.Mean)
+% hold on
+% plot(age_vec,AgeStats.healthy.earnings.Mean)
+% hold off
+% title('Earnings by health status')
+% xlabel('Age, j')
+% legend('Sick','All','Healthy')
+% 
+% % Worked hours h
+% figure
+% plot(age_vec,AgeStats.sick.hours.Mean)
+% hold on
+% plot(age_vec,AgeStats.hours.Mean)
+% hold on
+% plot(age_vec,AgeStats.healthy.hours.Mean)
+% hold off
+% title('Worked hours by health status')
+% xlabel('Age, j')
+% legend('Sick','All','Healthy')
+% 
+% % Assets
+% figure
+% plot(age_vec,AgeStats.sick.assets.Mean)
+% hold on
+% plot(age_vec,AgeStats.assets.Mean)
+% hold on
+% plot(age_vec,AgeStats.healthy.assets.Mean)
+% hold off
+% title('Assets by health status')
+% xlabel('Age, j')
+% legend('Sick','All','Healthy')
+% 
+% % Inequality for assets, whole population
+% cv_assets = AgeStats.assets.StdDev./max(AgeStats.assets.Mean,1e-12);
+% % At the start of life-cycle, everyone has zero assets, so std(assets)=0
+% % and mean(assets)=0, hence cv(assets)=NaN, but economically it should be
+% % zero
+% 
+% figure
+% plot(age_vec,AgeStats.assets.Gini)
+% title('Gini of Assets by age')
+% xlabel('Age, j')
+% 
+% figure
+% plot(age_vec,cv_assets)
+% title('Coefficient of variation of Assets by age')
+% xlabel('Age, j')
+% 
+% %% My checks
+% ave_by_health.earnings = [AllStats.sick.earnings.Mean,...
+%                           AllStats.earnings.Mean,...
+%                           AllStats.healthy.earnings.Mean];
+% ave_by_health.hours = [AllStats.sick.hours.Mean,...
+%                           AllStats.hours.Mean,...
+%                           AllStats.healthy.hours.Mean];
+% ave_by_health.assets = [AllStats.sick.assets.Mean,...
+%                           AllStats.assets.Mean,...
+%                           AllStats.healthy.assets.Mean];
+% gini_by_health.assets=[AllStats.sick.assets.Gini,...
+%                         AllStats.assets.Gini,...
+%                         AllStats.healthy.assets.Gini];
+% 
+% disp('SUMMARY STATISTICS')
+% fprintf('Average earnings <sick>    = %f \n',ave_by_health.earnings(1))
+% fprintf('Average earnings <all>     = %f \n',ave_by_health.earnings(2))
+% fprintf('Average earnings <healthy> = %f \n',ave_by_health.earnings(3))
+% disp('---')
+% fprintf('Average hours <sick>    = %f \n',ave_by_health.hours(1))
+% fprintf('Average hours <all>     = %f \n',ave_by_health.hours(2))
+% fprintf('Average hours <healthy> = %f \n',ave_by_health.hours(3))
+% disp('---')
+% fprintf('Average assets <sick>    = %f \n',ave_by_health.assets(1))
+% fprintf('Average assets <all>     = %f \n',ave_by_health.assets(2))
+% fprintf('Average assets <healthy> = %f \n',ave_by_health.assets(3))
+% disp('---')
+% fprintf('Gini assets <sick>    = %f \n',gini_by_health.assets(1))
+% fprintf('Gini assets <all>     = %f \n',gini_by_health.assets(2))
+% fprintf('Gini assets <healthy> = %f \n',gini_by_health.assets(3))
+% 
+%(a,z1,z2,e,theta_i,j)
+mu_age = reshape(sum(mu,[1,2,3,4,5]),1,Params.J);
 
-% Assets
-figure
-plot(age_vec,AgeStats.sick.assets.Mean)
-hold on
-plot(age_vec,AgeStats.assets.Mean)
-hold on
-plot(age_vec,AgeStats.healthy.assets.Mean)
-hold off
-title('Assets by health status')
-xlabel('Age, j')
-legend('Sick','All','Healthy')
+disp('Check age marginal distribution')
+disp(max(abs(mu_age-Params.mewj)))
 
-% Inequality for assets, whole population
-cv_assets = AgeStats.assets.StdDev./max(AgeStats.assets.Mean,1e-12);
-% At the start of life-cycle, everyone has zero assets, so std(assets)=0
-% and mean(assets)=0, hence cv(assets)=NaN, but economically it should be
-% zero
+disp('Check share of sick in whole pop.')
+share_sick1 = AllStats.share_sick.Mean; 
+share_sick2 = sum(AgeStats.share_sick.Mean.*Params.mewj);
+disp([share_sick1,share_sick2])
+ 
+disp('Check mean assets conditional on health=sick')
+xx1 = AllStats.sick.assets.Mean;
+xx2 = sum(AgeStats.sick.assets.Mean.*AgeStats.share_sick.Mean.*Params.mewj)/share_sick2;
+age_sick_share2 = AgeStats.share_sick.Mean.*Params.mewj;
+% My calculation
+Values_assets = repmat(a_grid,[1,n_z(1),n_z(2),n_e,n_theta,N_j]);
+age_sick_share3 = squeeze(sum(mu(:,:,1,:,:,:),[1,2,3,4,5]));
+age_sick_diff23 = max(abs(age_sick_share2'-age_sick_share3));
+asset_age_sick3 = squeeze(sum(Values_assets(:,:,1,:,:,:).*mu(:,:,1,:,:,:),[1,2,3,4,5]))...
+    ./squeeze(sum(mu(:,:,1,:,:,:),[1,2,3,4,5]));
+asset_age_sick_diff23 = max(abs(AgeStats.sick.assets.Mean'-asset_age_sick3));
 
-figure
-plot(age_vec,AgeStats.assets.Gini)
-title('Gini of Assets by age')
-xlabel('Age, j')
+Value_aprime = zeros([n_a,n_z(1),n_z(2),n_e,n_theta,N_j]);
 
-figure
-plot(age_vec,cv_assets)
-title('Coefficient of variation of Assets by age')
-xlabel('Age, j')
-
-%% My checks
-ave_by_health.earnings = [AllStats.sick.earnings.Mean,...
-                          AllStats.earnings.Mean,...
-                          AllStats.healthy.earnings.Mean];
-ave_by_health.hours = [AllStats.sick.hours.Mean,...
-                          AllStats.hours.Mean,...
-                          AllStats.healthy.hours.Mean];
-ave_by_health.assets = [AllStats.sick.assets.Mean,...
-                          AllStats.assets.Mean,...
-                          AllStats.healthy.assets.Mean];
-gini_by_health.assets=[AllStats.sick.assets.Gini,...
-                        AllStats.assets.Gini,...
-                        AllStats.healthy.assets.Gini];
-
-disp('SUMMARY STATISTICS')
-fprintf('Average earnings <sick>    = %f \n',ave_by_health.earnings(1))
-fprintf('Average earnings <all>     = %f \n',ave_by_health.earnings(2))
-fprintf('Average earnings <healthy> = %f \n',ave_by_health.earnings(3))
-disp('---')
-fprintf('Average hours <sick>    = %f \n',ave_by_health.hours(1))
-fprintf('Average hours <all>     = %f \n',ave_by_health.hours(2))
-fprintf('Average hours <healthy> = %f \n',ave_by_health.hours(3))
-disp('---')
-fprintf('Average assets <sick>    = %f \n',ave_by_health.assets(1))
-fprintf('Average assets <all>     = %f \n',ave_by_health.assets(2))
-fprintf('Average assets <healthy> = %f \n',ave_by_health.assets(3))
-disp('---')
-fprintf('Gini assets <sick>    = %f \n',gini_by_health.assets(1))
-fprintf('Gini assets <all>     = %f \n',gini_by_health.assets(2))
-fprintf('Gini assets <healthy> = %f \n',gini_by_health.assets(3))
-
-%% Last check: compare conditional earnings given health=sick and given age
-
-%income_sick1 = AgeStats.sick.earnings.Mean;
-
-Values = zeros(size(mu));
-Values_assets =  zeros(size(mu));
 for j=1:N_j
-for theta_c=1:N_i
+for theta_c=1:n_theta
 for e_c=1:n_e
 for z2_c=1:n_z(2)
 for z1_c=1:n_z(1)
 for a_c=1:n_a
-    h = d_grid(Policy_cpu(1,a_c,z1_c,z2_c,e_c,theta_c,j));
     aprime = a_grid(Policy_cpu(2,a_c,z1_c,z2_c,e_c,theta_c,j));
-    a_val = a_grid(a_c);
-    z1 = z1_grid(z1_c);
-    z2 = z2_grid(z2_c);
-    e = e_grid(e_c);
-    theta = Params.theta_i(theta_c);
-    Values_assets(a_c,z1_c,z2_c,e_c,theta_c,j) = FnsToEvaluate.assets(h,aprime,a_val,z1,z2,e);
-    Values(a_c,z1_c,z2_c,e_c,theta_c,j) = ...
-        FnsToEvaluate.earnings(h,aprime,a_val,z1,z2,e,theta,Params.kappa_j(j),...
-        Params.w,Params.agej(j),Params.Jr,Params.pen,Params.cut);
+    Value_aprime(a_c,z1_c,z2_c,e_c,theta_c,j) = aprime;
+  
 end
 end
 end
 end
 end
 end
+asset_age_sick4 = squeeze(sum(Value_aprime(:,:,1,:,:,:).*mu(:,:,1,:,:,:),[1,2,3,4,5]))...
+    ./squeeze(sum(mu(:,:,1,:,:,:),[1,2,3,4,5]));
+asset_age_sick_diff24 = max(abs(AgeStats.sick.assets.Mean'-asset_age_sick4));
 
-income_sick2 = zeros(1,N_j);
-for j=1:N_j
-    num = sum(Values(:,:,1,:,:,j).*mu(:,:,1,:,:,j),"all");
-    den = sum(mu(:,:,1,:,:,j),"all");
-    income_sick2(1,j) = num/den;
-end
-
-%err = abs(income_sick2-income_sick1);
-
-%disp('Earnings by age, given sick: Error toolkit vs my code:')
-%disp(max(err))
-
-% Average earnings given sick, in the whole population
-num = sum(Values(:,:,1,:,:,:).*mu(:,:,1,:,:,:),"all");
-den = sum(mu(:,:,1,:,:,:),"all"); %divide by mass of sick people
-ave_earnings_sick_cpu = num/den;
-
-fprintf('Average earnings, given sick, toolkit = %f \n',AllStats.sick.earnings.Mean)
-fprintf('Average earnings, given sick, CPU     = %f \n',ave_earnings_sick_cpu)
-
-% Average assets given sick/healthy, whole population - AllStats.sick.assets.Mean
-%   (a,z1,z2,e,theta_i,j)
-num = sum(Values_assets(:,:,1,:,:,:).*mu(:,:,1,:,:,:),"all");
-den = sum(mu(:,:,1,:,:,:),"all");
-ave_assets_sick_cpu = num/den;
-num = sum(Values_assets(:,:,2,:,:,:).*mu(:,:,2,:,:,:),"all");
-den = sum(mu(:,:,2,:,:,:),"all");
-ave_assets_healthy_cpu = num/den;
-
-[ave_assets_sick_cpu,ave_assets_healthy_cpu]
-[AllStats.sick.assets.Mean,AllStats.healthy.assets.Mean]
-
-%% Another check
-% Average assets given health=sick
-xx1 = AllStats.sick.assets.Mean; 
-xx2 = sum(AgeStats.sick.assets.Mean.*AgeStats.share_sick.Mean.*AgeStats.agej.Mean)/(sum(AgeStats.share_sick.Mean.*AgeStats.agej.Mean));
-[xx1,xx2]
-
-disp('RUNNING TIMES FOR SUBPARTS OF PROGRAM:')
-disp('simoptions.whichstats = ')
-disp(simoptions.whichstats)
-fprintf('Time for VFI: %f \n',time_vfi)
-fprintf('Time for Distr: %f \n',time_dist)
-fprintf('Time for AgeStats: %f \n',time_age)
-fprintf('Time for AllStats: %f \n',time_all)
+% %(a,z1,z2,e,theta_i,j)
+% xx3 = sum(Values_assets(:,:,1,:,:,:).*mu(:,:,1,:,:,:),"all")/sum(mu(:,:,1,:,:,:),"all");
+% disp([xx1,xx2,xx3])
+% 
+% 
+% 
+% yy1 = AllStats.assets.Mean;
+% yy2 = sum(AgeStats.assets.Mean.*Params.mewj);
+% yy3 = sum(Values_assets.*mu,"all");
+% 
+% disp('Check mean earnings conditional on health=sick')
+% xx4 = AllStats.sick.earnings.Mean;
+% xx5 = sum(AgeStats.sick.earnings.Mean.*AgeStats.share_sick.Mean.*Params.mewj)/share_sick2;
+% disp([xx4,xx5])
+% 
+% disp('Check mean earnings conditional on health=healthy')
+% xx6 = AllStats.healthy.earnings.Mean;
+% xx7 = sum(AgeStats.healthy.earnings.Mean.*(1-AgeStats.share_sick.Mean).*Params.mewj)/(1-share_sick2);
+% disp([xx6,xx7])
+% 
+% % %% Last check: compare conditional earnings given health=sick and given age
+% % 
+% % %income_sick1 = AgeStats.sick.earnings.Mean;
+% % 
+% % Values = zeros(size(mu));
+% % Values_assets =  zeros(size(mu));
+% % for j=1:N_j
+% % for theta_c=1:N_i
+% % for e_c=1:n_e
+% % for z2_c=1:n_z(2)
+% % for z1_c=1:n_z(1)
+% % for a_c=1:n_a
+% %     h = d_grid(Policy_cpu(1,a_c,z1_c,z2_c,e_c,theta_c,j));
+% %     aprime = a_grid(Policy_cpu(2,a_c,z1_c,z2_c,e_c,theta_c,j));
+% %     a_val = a_grid(a_c);
+% %     z1 = z1_grid(z1_c);
+% %     z2 = z2_grid(z2_c);
+% %     e = e_grid(e_c);
+% %     theta = Params.theta_i(theta_c);
+% %     Values_assets(a_c,z1_c,z2_c,e_c,theta_c,j) = FnsToEvaluate.assets(h,aprime,a_val,z1,z2,e);
+% %     Values(a_c,z1_c,z2_c,e_c,theta_c,j) = ...
+% %         FnsToEvaluate.earnings(h,aprime,a_val,z1,z2,e,theta,Params.kappa_j(j),...
+% %         Params.w,Params.agej(j),Params.Jr,Params.pen,Params.cut);
+% % end
+% % end
+% % end
+% % end
+% % end
+% % end
+% % 
+% % income_sick2 = zeros(1,N_j);
+% % for j=1:N_j
+% %     num = sum(Values(:,:,1,:,:,j).*mu(:,:,1,:,:,j),"all");
+% %     den = sum(mu(:,:,1,:,:,j),"all");
+% %     income_sick2(1,j) = num/den;
+% % end
+% % 
+% % %err = abs(income_sick2-income_sick1);
+% % 
+% % %disp('Earnings by age, given sick: Error toolkit vs my code:')
+% % %disp(max(err))
+% % 
+% % % Average earnings given sick, in the whole population
+% % num = sum(Values(:,:,1,:,:,:).*mu(:,:,1,:,:,:),"all");
+% % den = sum(mu(:,:,1,:,:,:),"all"); %divide by mass of sick people
+% % ave_earnings_sick_cpu = num/den;
+% % 
+% % fprintf('Average earnings, given sick, toolkit = %f \n',AllStats.sick.earnings.Mean)
+% % fprintf('Average earnings, given sick, CPU     = %f \n',ave_earnings_sick_cpu)
+% % 
+% % % Average assets given sick/healthy, whole population - AllStats.sick.assets.Mean
+% % %   (a,z1,z2,e,theta_i,j)
+% % num = sum(Values_assets(:,:,1,:,:,:).*mu(:,:,1,:,:,:),"all");
+% % den = sum(mu(:,:,1,:,:,:),"all");
+% % ave_assets_sick_cpu = num/den;
+% % num = sum(Values_assets(:,:,2,:,:,:).*mu(:,:,2,:,:,:),"all");
+% % den = sum(mu(:,:,2,:,:,:),"all");
+% % ave_assets_healthy_cpu = num/den;
+% % 
+% % [ave_assets_sick_cpu,ave_assets_healthy_cpu]
+% % [AllStats.sick.assets.Mean,AllStats.healthy.assets.Mean]
+% % 
+% % %% Another check
+% % % Average assets given health=sick
+% % 
+% 
+% disp('RUNNING TIMES FOR SUBPARTS OF PROGRAM:')
+% disp('simoptions.whichstats = ')
+% if isfield(simoptions,'whichstats')
+%     disp(simoptions.whichstats)
+% else
+%     disp('Default options')
+% end
+% fprintf('Time for VFI: %f \n',time_vfi)
+% fprintf('Time for Distr: %f \n',time_dist)
+% fprintf('Time for AgeStats: %f \n',time_age)
+% fprintf('Time for AllStats: %f \n',time_all)
--- ALE/reshape_VandPolicy.m
+++ ALE/reshape_VandPolicy.m
@@ -3,25 +3,30 @@ function [mu,Policy] = reshape_VandPolicy(StatDist,Policy,n_a,n_z,n_e,N_i,N_j)
 % Assumptions: 
 % only 2 permanent types (low and high), 
 % 2 z shocks, 
-% no d variable
 if iscell(N_i)
     N_i = numel(N_i);
 end
 
-mysize = [n_a,n_z(1),n_z(2),n_e];
+mysize = [n_a,n_z(1),n_z(2),n_e]; %without perm type and without age
 
-StatDist_1 = gather(StatDist.ptype001);
-StatDist_2 = gather(StatDist.ptype002);
-Policy_1 = squeeze(gather(Policy.ptype001));
-Policy_2 = squeeze(gather(Policy.ptype002));
+StatDist_1 = gather(StatDist.low); % (a,z1,z2,e,j)
+StatDist_2 = gather(StatDist.high);
+Policy_1 = squeeze(gather(Policy.low));
+Policy_2 = squeeze(gather(Policy.high));
 
 if ~isequal(size(Policy_1),[2,mysize,N_j])
     error('Policy has wrong size')
 end
 
 mu = zeros(n_a,n_z(1),n_z(2),n_e,N_i,N_j);
-mu(:,:,:,:,1,:) = StatDist_1*StatDist.ptweights(1);
-mu(:,:,:,:,2,:) = StatDist_2*StatDist.ptweights(2);
+%mu(:,:,:,:,1,:) = StatDist_1*StatDist.ptweights(1);
+%mu(:,:,:,:,2,:) = StatDist_2*StatDist.ptweights(2);
+for j=1:N_j
+    mu(:,:,:,:,1,j) = StatDist_1(:,:,:,:,j)*StatDist.ptweights(1);
+    mu(:,:,:,:,2,j) = StatDist_2(:,:,:,:,j)*StatDist.ptweights(2);
+
+end
+
 Policy = zeros(2,n_a,n_z(1),n_z(2),n_e,N_i,N_j);
 Policy(:,:,:,:,:,1,:) = Policy_1;
 Policy(:,:,:,:,:,2,:) = Policy_2;
